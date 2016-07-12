f6b86169ea59dc574c00acc01552146f79b976e9
slewis <slewis@composent.com>
2016-07-12 13:04:35 -0700

    Improvements to Python servicebridge API

--- bundles/org.eclipse.ecf.provider.py4/python/src/servicebridge/__init__.py
+++ bundles/org.eclipse.ecf.provider.py4/python/src/servicebridge/__init__.py
@@ -1,5 +1,111 @@
 
+from uuid import UUID, uuid1
+from time import time
+from datetime import datetime
+from threading import Lock
+from argparse import ArgumentError
 
-_RSA_ENDPOINT_ID = 'endpoint.id'
-_JAVA_DIRECT_ENDPOINT_CLASS = 'org.eclipse.ecf.provider.direct.DirectRemoteServiceProvider'
+#----------------------------------------------------------------------------
+# RSA constants (declared in org.osgi.service.remoteserviceadmin.RemoteConstants
+ENDPOINT_ID = "endpoint.id"
+ENDPOINT_SERVICE_ID = "endpoint.service.id"
+ENDPOINT_FRAMEWORK_UUID = "endpoint.framework.uuid"
+ENDPOINT_PACKAGE_VERSION_ = "endpoint.package.version."
+SERVICE_EXPORTED_INTERFACES = "service.exported.interfaces"
+REMOTE_CONFIGS_SUPPORTED = "remote.configs.supported"
+REMOTE_INTENTS_SUPPORTED = "remote.intents.supported"
+SERVICE_EXPORTED_CONFIGS = "service.exported.configs"
+SERVICE_EXPORTED_INTENTS = "service.exported.intents"
+SERVICE_EXPORTED_INTENTS_EXTRA = "service.exported.intents.extra"
+SERVICE_IMPORTED = "service.imported"
+SERVICE_IMPORTED_CONFIGS = "service.imported.configs"
+SERVICE_INTENTS = "service.intents"
+SERVICE_ID = "service.id"
+
+ECF_ENDPOINT_CONTAINERID_NAMESPACE = "ecf.endpoint.id.ns"
+ECF_ENDPOINT_ID = "ecf.endpoint.id"
+ECF_RSVC_ID = "ecf.rsvc.id"
+ECF_ENDPOINT_TIMESTAMP = "ecf.endpoint.ts"
+ECF_ENDPOINT_CONNECTTARGET_ID = "ecf.endpoint.connecttarget.id"
+ECF_ENDPOINT_IDFILTER_IDS = "ecf.endpoint.idfilter.ids"
+
+ECF_ENDPOINT_REMOTESERVICE_FILTER = "ecf.endpoint.rsfilter"
+ECF_SERVICE_EXPORTED_CONTAINER_FACTORY_ARGS = "ecf.exported.containerfactoryargs"
+ECF_SERVICE_EXPORTED_CONTAINER_CONNECT_CONTEXT = "ecf.exported.containerconnectcontext"
+ECF_SERVICE_EXPORTED_CONTAINER_ID = "ecf.exported.containerid"
+ECF_SERVICE_EXPORTED_ASYNC_INTERFACES = "ecf.exported.async.interfaces"
+ECF_SERVICE_EXPORTED_ASYNC_NOPROXY = "ecf.rsvc.async.noproxy"
+ECF_SERVICE_ASYNC_RSPROXY_CLASS_ = "ecf.rsvc.async.proxy_"
+ECF_ASYNC_INTERFACE_SUFFIX = "Async"
+ECF_SERVICE_IMPORTED_VALUETYPE = "ecf.service.imported.valuetype"
+ECF_SERVICE_IMPORTED_ENDPOINT_ID = ENDPOINT_ID
+ECF_SERVICE_IMPORTED_ENDPOINT_SERVICE_ID = ENDPOINT_SERVICE_ID
+ECF_OSGI_ENDPOINT_MODIFIED = "ecf.osgi.endpoint.modified"
+ECF_OSGI_CONTAINER_ID_NS = "ecf.osgi.ns"
+#----------------------------------------------------------------------------------
+
+def create_uuid():
+    return str(uuid1())
+
+def time_since_epoch():
+    return int(time() * 1000)
+
+_next_rsid = 1
+_next_rsid_lock = Lock()
+
+def get_next_rsid():
+    with _next_rsid_lock:
+        global _next_rsid
+        n = _next_rsid
+        _next_rsid += 1
+        return n
+
+def merge_dicts(*dict_args):
+    '''
+    Given any number of dicts, shallow copy and merge into a new dict,
+    precedence goes to key value pairs in latter dicts.
+    '''
+    result = {}
+    for dictionary in dict_args:
+        result.update(dictionary)
+    return result
+
+def get_rsa_props(object_class, rmt_configs, ep_svc_id=None, fw_id=None, pkg_ver=None):
+    results = {}
+    if not object_class:
+        raise ArgumentError('object_class must be an [] of Strings')
+    results['objectClass'] = object_class
+    if not rmt_configs:
+        raise ArgumentError('rmt_configs must be an array of Strings')
+    results[REMOTE_CONFIGS_SUPPORTED] = rmt_configs
+    results[SERVICE_IMPORTED_CONFIGS] = rmt_configs
+    if not ep_svc_id:
+        ep_svc_id = get_next_rsid()
+    results[ENDPOINT_SERVICE_ID] = ep_svc_id
+    results[SERVICE_ID] = ep_svc_id
+    if not fw_id:
+        fw_id = create_uuid()
+    results[ENDPOINT_FRAMEWORK_UUID] = fw_id
+    if pkg_ver:
+        results[ENDPOINT_PACKAGE_VERSION_+pkg_ver[0]] = pkg_ver[1]
+    results[ENDPOINT_ID] = create_uuid()
+    results[SERVICE_IMPORTED] = True
+    return results
+
+def get_ecf_props(ep_id, ep_id_ns, rsvc_id=None, ep_ts=None):
+    results = {}
+    if not ep_id:
+        raise ArgumentError('ep_id must be a valid endpoint id')
+    results[ECF_ENDPOINT_ID] = ep_id
+    if not ep_id_ns:
+        raise ArgumentError('ep_id_ns must be a valid namespace')
+    results[ECF_ENDPOINT_CONTAINERID_NAMESPACE] = ep_id_ns
+    if not rsvc_id:
+        rsvc_id = get_next_rsid()
+    results[ECF_RSVC_ID] = rsvc_id
+    if not ep_ts:
+        ep_ts = time_since_epoch()
+    results[ECF_ENDPOINT_TIMESTAMP] = ep_ts
+    return results
+
 
--- bundles/org.eclipse.ecf.provider.py4/python/src/servicebridge/py4j.py
+++ bundles/org.eclipse.ecf.provider.py4/python/src/servicebridge/py4j.py
@@ -6,10 +6,22 @@ Created on Jul 11, 2016
 from logging import getLogger as getLibLogger
 from threading import RLock
 
-from py4j.java_gateway import JavaGateway
-from py4j.java_gateway import CallbackServerParameters
+
+from py4j.java_gateway import JavaGateway, CallbackServerParameters
 from py4j.java_collections import ListConverter, MapConverter
-from servicebridge import _RSA_ENDPOINT_ID, _JAVA_DIRECT_ENDPOINT_CLASS
+from servicebridge import merge_dicts, ENDPOINT_ID
+
+PY4J_EXPORTED_CONFIGS = ['ecf.py4j.host.python']
+PY4J_PROTOCOL = 'py4j'
+PY4J_DEFAULT_GATEWAY_PORT = 25333
+PY4J_DEFAULT_CB_PORT = 25334
+PY4J_DEFAULT_HOSTNAME = 'localhost'
+PY4J_PYTHON_PATH = "/python"
+PY4J_JAVA_PATH = "/java"
+
+PY4J_NAMESPACE = 'ecf.namespace.py4j'
+JAVA_DIRECT_ENDPOINT_CLASS = 'org.eclipse.ecf.provider.direct.DirectRemoteServiceProvider'
+PY4J_DEFAULT_REMOTE_CONFIGS = ['passByReference', 'exactlyOnce', 'ordered']
 
 # Version
 __version_info__ = (0, 1, 0)
@@ -24,19 +36,41 @@ _logger = getLibLogger(__name__)
 
 # ------------------------------------------------------------------------------
 
-class Py4jBridgeEventListener(object):
-    def service_exported(self,endpointid,endpoint):
-        pass
+def make_edef_props(*args,**kwargs):
+    dicts = [x for x in args if isinstance(x,type({}))]
+    itemdict = {k: v for k, v in kwargs.items() if k not in dicts}
+    dicts.append(itemdict)
+    tpl = tuple(dicts)
+    return merge_dicts(*tpl)
+
+def createLocalPy4jId(hostname=PY4J_DEFAULT_HOSTNAME,port=PY4J_DEFAULT_CB_PORT):
+    return PY4J_PROTOCOL + "://" + hostname + ":" + str(port) + PY4J_PYTHON_PATH
+
+def createJavaPy4jId(hostname=PY4J_DEFAULT_HOSTNAME,port=PY4J_DEFAULT_GATEWAY_PORT):
+    return PY4J_PROTOCOL + "://" + hostname + ":" + str(port) + PY4J_JAVA_PATH
+
+class Py4jServiceBridgeEventListener(object):
+    '''
+    Listener adapter class.   Subclasses can set themselves up
+    as a listener for Py4jBridge events (service_imported, service_modified,
+    service_unimported see below), buy extending this class and overriding
+    these methods, and then providing the listener to the Py4jBridge instance
+    creation'''
+    def service_imported(self,endpointid,endpoint):
+        _logger.info('_service_imported endpointid='+endpointid)
     
     def service_modified(self,endpointid,endpoint):
-        pass
+        _logger.info('_service_modified endpointid='+endpointid)
     
-    def service_unexported(self,endpointid,endpoint):
-        pass
+    def service_unimported(self,endpointid,endpoint):
+        _logger.info('_service_unimported endpointid='+endpointid)
     
     
-class Py4jBridge(object):
-    
+class Py4jServiceBridge(object):
+    '''Py4jServiceBridge class
+    This class provides and API for consumers to use the Py4jServiceBridge.  This 
+    allows a bridge between Python and the OSGi service registry.
+    '''
     def __init__(self,listener=None):
         self._gateway = None
         self._lock = RLock()
@@ -46,6 +80,52 @@ class Py4jBridge(object):
         self._map_converter = MapConverter()
         self._list_converter = ListConverter()
         self._listener = listener
+    
+    def _import_service_from_java(self,proxy,props):
+        with self._endpoints_lock:
+            try:
+                endpointid = props[ENDPOINT_ID]
+                endpoint = (proxy,props)
+                self._endpoints[endpointid] = endpoint
+                if not self._listener is None:
+                    try:
+                        self._listener.service_imported(endpointid,endpoint)
+                    except:
+                        _logger.error('_import_service_from_java listener threw exception endpointid='+endpointid)
+            except KeyError:
+                pass
+    def _modify_service_from_java(self,props):
+        with self._endpoints_lock:
+            try:
+                endpointid = props[ENDPOINT_ID]
+                endpoint = self._endpoints[endpointid]
+                endpoint[1].update(props)
+                if not self._listener is None:
+                    try:
+                        self._listener.service_modified(endpointid,endpoint)
+                    except:
+                        _logger.error('_modify_service_from_java listener threw exception endpointid='+endpointid)
+            except KeyError:
+                pass
+    
+    def _unimport_service_from_java(self,props):
+        with self._endpoints_lock:
+            try:
+                endpointid = props[ENDPOINT_ID]
+                endpoint = self._endpoints.pop(endpointid)
+                if not self._listener is None:
+                    try:
+                        self._listener.service_removed(endpointid,endpoint)
+                    except:
+                        _logger.error('_unimport_service_from_java listener threw exception endpointid='+endpointid)
+                        pass
+            except KeyError:
+                pass
+
+    def get_jvm(self):
+        with self._lock:
+            self._raise_not_connected();
+            return self._gateway.jvm
         
     def get_endpoint(self,endpointid):
         with self._endpoints_lock:
@@ -64,10 +144,10 @@ class Py4jBridge(object):
             result = {}
             for item in props.items():
                 val = item[1]
-                if isinstance(val, type[[]]):
+                if isinstance(val, type([])):
                     val = self._list_converter.convert(val,self._gateway._gateway_client)
-            result[item[0]] = val
-            return self._map_converter.convert(props,self._gateway._gateway_client)
+                result[item[0]] = val
+            return self._map_converter.convert(result,self._gateway._gateway_client)
 
     def isconnected(self):
         with self._lock:
@@ -81,79 +161,49 @@ class Py4jBridge(object):
                 raise ConnectionError('already connected to java gateway')
             self._gateway = JavaGateway(callback_server_parameters=callback_server_parameters)
             self._consumer = self._gateway.entry_point.getJavaConsumer()
-            self._gateway.entry_point.setPythonConsumer(self)
+            class JavaRemoteServiceExporter(object):
+                def __init__(self, bridge):
+                    self._bridge = bridge
+                    
+                def exportService(self,proxy,props):
+                    self._bridge._import_service_from_java(proxy,props)
+                    
+                def modifyService(self,props):
+                    self._bridge._modify_service_from_java(props)
+                    
+                def unexportService(self,props):
+                    self._bridge._unimport_service_from_java(props)
+                class Java:
+                    implements = [JAVA_DIRECT_ENDPOINT_CLASS]
+
+            self._bridge = JavaRemoteServiceExporter(self)
+            self._gateway.entry_point.setPythonConsumer(self._bridge)
     
     def _raise_not_connected(self):
         if not self.isconnected():
             raise ConnectionError('Not connected to java gateway')
-        
-    def export_to_java(self,svc,props):
+
+    def get_access(self):
+        return createLocalPy4jId(port=self._gateway.get_callback_server().get_listening_port())
+    
+    def export_to_java(self,svc,export_props):
         with self._lock:
             self._raise_not_connected()
-            self._consumer.exportService(svc,self._convert_map(props))
+            props = self.convert_props_for_java(export_props)
+            self._consumer.exportService(svc,props)
 
     def unexport_to_java(self,props):
         with self._lock:
             self._raise_not_connected()
-        self._consumer.unexportService(self._convert_map(props))
+        self._consumer.unexportService(self.convert_props_for_java(props))
 
     def disconnect(self):
         with self._lock:
             if self.isconnected():
                 self._gateway.shutdown()
                 self._gateway = None
-                self._consumer = None    
+                self._consumer = None   
+                self._bridge = None 
         with self._endpoints_lock:
             self._endpoints.clear()
      
-    class Java:
-        implements = [_JAVA_DIRECT_ENDPOINT_CLASS]
-
-    def exportService(self,proxy,props):
-        with self._endpoints_lock:
-            try:
-                endpointid = props[_RSA_ENDPOINT_ID]
-                print('adding endpointid='+endpointid+",proxy="+proxy.toString()+",props="+props.toString())
-                endpoint = (proxy,props)
-                self._endpoints[endpointid] = endpoint
-                if not self._listener is None:
-                    try:
-                        self._listener.service_exported(endpointid,endpoint)
-                    except:
-                        # log?
-                        pass
-            except KeyError:
-                pass
-            
-
-    def modifyService(self,props):
-        with self._endpoints_lock:
-            try:
-                endpointid = props[_RSA_ENDPOINT_ID]
-                endpoint = self._endpoints[endpointid]
-                endpoint[1].update(props)
-                if not self._listener is None:
-                    try:
-                        self._listener.service_modified(endpointid,endpoint)
-                    except:
-                        # log?
-                        pass
-            except KeyError:
-                pass
-        
-    def unexportService(self,props):
-        with self._endpoints_lock:
-            try:
-                endpointid = props[_RSA_ENDPOINT_ID]
-                print('removing endpointid='+endpointid)
-                endpoint = self._endpoints.pop(endpointid)
-                if not self._listener is None:
-                    try:
-                        self._listener.service_removed(endpointid,endpoint)
-                    except:
-                        # log?
-                        pass
-            except KeyError:
-                pass
-        
-
