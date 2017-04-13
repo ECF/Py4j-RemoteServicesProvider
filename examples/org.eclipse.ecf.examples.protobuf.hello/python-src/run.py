e0ee58597887fbe1af3f8e92ab1abb82f25a22dc
Scott Lewis <scottx.lewis@intel.com>
2017-04-12 17:49:56 -0700

    Added decorator and PBService super class

--- examples/org.eclipse.ecf.examples.protobuf.hello/python-src/run.py
+++ examples/org.eclipse.ecf.examples.protobuf.hello/python-src/run.py
@@ -5,34 +5,50 @@ Created on Apr 11, 2017
 '''
 from osgiservicebridge.bridge import Py4jServiceBridge
 from hellomsg_pb2 import HelloMsgContent
+from functools import wraps
 
-class MyClass(object):
-    
+def pbdecorator(func):
+    @wraps(func)
+    def decorate(*args,**kwargs):
+        argClass = HelloMsgContent
+        if len(args) > 1 and argClass:
+            argInst = argClass()
+            argInst.ParseFromString(args[1])
+        respb = func(args[0],argInst)
+        resBytes = None
+        if respb:
+            resBytes = respb.SerializeToString()
+        return resBytes
+    return decorate
+
+class PBService(object):
     
     def _raw_bytes_from_java(self,methodName,serializedArgs):
-        #print("methodName="+str(methodName))
-        #print("serializedArgs="+str(serializedArgs))
-        pbarg = HelloMsgContent()
-        pbarg.ParseFromString(serializedArgs)
-        print("input arg="+str(pbarg))
-        
-        # do something with pbarg
-        
-        # now create a response
-        msgcontent = HelloMsgContent()
-        msgcontent.h = 'phere'
-        msgcontent.f = 'pslewis'
-        msgcontent.to = 'jslewis'
-        msgcontent.hellomsg = 'hi from python'
-        for x in range(0,5):
-            msgcontent.x.append(float(x))
-        return msgcontent.SerializeToString()
+        lastdot = methodName.rfind('.')
+        if lastdot >= 0:
+            methodName = methodName[lastdot+1:]
+        return getattr(self,methodName)(serializedArgs)
     
     class Java:
         implements = ['org.eclipse.ecf.examples.protobuf.hello.IHello']
         service_exported_configs = ['ecf.py4j.host.python.pb']
 
+class MyClass(PBService):
+    
+    @pbdecorator
+    def sayHello(self,pbarg):
+        print("sayHello called with arg="+str(pbarg))
+        resmsg = HelloMsgContent()
+        resmsg.h = 'phere'
+        resmsg.f = 'pslewis'
+        resmsg.to = 'jslewis'
+        resmsg.hellomsg = 'hi from python'
+        for x in range(0,5):
+            resmsg.x.append(float(x))
+        return resmsg
+
 if __name__ == '__main__':
+    inst = MyClass()
     bridge = Py4jServiceBridge()
     print("bridge created")
     bridge.connect()
