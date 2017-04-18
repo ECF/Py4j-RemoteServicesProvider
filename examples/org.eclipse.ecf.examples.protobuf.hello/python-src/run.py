6500fa63880046f8bef2a5767c7d95541a0c8670
slewis <slewis@composent.com>
2017-04-18 12:18:20 -0700

    Removed embedded osgiservicebridge.

--- examples/org.eclipse.ecf.examples.protobuf.hello/python-src/run.py
+++ examples/org.eclipse.ecf.examples.protobuf.hello/python-src/run.py
@@ -4,37 +4,13 @@ Created on Apr 11, 2017
 @author: slewis
 '''
 from osgiservicebridge.bridge import Py4jServiceBridge
+from osgiservicebridge.protobuf import ProtobufServiceImpl, protobufmethod, PB_SERVICE_EXPORTED_CONFIGS_DEFAULT
+
 from hellomsg_pb2 import HelloMsgContent
-from functools import wraps
 
-def pbdecorator(arg_type):
-    def pbwrapper(func):
-        @wraps(func)
-        def wrapper(*args,**kwargs):
-            argClass = arg_type
-            if len(args) > 1 and argClass:
-                argInst = argClass()
-                argInst.ParseFromString(args[1])
-            respb = func(args[0],argInst)
-            resBytes = None
-            if respb:
-                resBytes = respb.SerializeToString()
-            return resBytes
-        return wrapper
-    return pbwrapper
-
-class PBService(object):
+class HelloServiceImpl(ProtobufServiceImpl):
     
-    def _raw_bytes_from_java(self,methodName,serializedArgs):
-        return getattr(self,methodName)(serializedArgs)
-    
-    class Java:
-        implements = ['org.eclipse.ecf.examples.protobuf.hello.IHello']
-        service_exported_configs = ['ecf.py4j.host.python.pb']
-
-class HelloServiceImpl(PBService):
-    
-    @pbdecorator(arg_type=HelloMsgContent)
+    @protobufmethod(arg_type=HelloMsgContent)
     def sayHello(self,pbarg):
         print("sayHello called with arg="+str(pbarg))
         resmsg = HelloMsgContent()
@@ -45,6 +21,10 @@ class HelloServiceImpl(PBService):
         for x in range(0,5):
             resmsg.x.append(float(x))
         return resmsg
+    
+    class Java:
+        implements = ['org.eclipse.ecf.examples.protobuf.hello.IHello']
+        service_exported_configs = PB_SERVICE_EXPORTED_CONFIGS_DEFAULT
 
 if __name__ == '__main__':
     bridge = Py4jServiceBridge()
