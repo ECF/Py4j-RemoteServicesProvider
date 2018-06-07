ca0d4f95abc47a659be7da56850009440c534ed8
slewis <slewis@composent.com>
2018-06-07 13:50:35 -0700

    Updated to 1.4 of osgiservicebridge

--- python/osgiservicebridge/setup.py
+++ python/osgiservicebridge/setup.py
@@ -22,7 +22,7 @@ exec(compile(open(VERSION_PATH).read(),
      VERSION_PATH, "exec"))
 VERSION = __version__  # noqa
 
-JAVA_FILE='java/osgiservicebridge-java-bundles_2.7.2.zip'
+JAVA_FILE='java/osgiservicebridge-java-bundles_2.8.0.zip'
 
 setup(
     name='osgiservicebridge',
